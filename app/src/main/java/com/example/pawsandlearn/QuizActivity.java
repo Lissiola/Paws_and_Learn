e5a87c13bcc069efd8ff9742bfb618eb8394201b
alainaburns <burnsal@udmercy.edu>
2025-04-02 15:49:03 -0400

    Merge remote-tracking branch 'origin/AlainasBranch' into AlainasBranch

--- app/src/main/java/com/example/pawsandlearn/QuizActivity.java
+++ app/src/main/java/com/example/pawsandlearn/QuizActivity.java
@@ -1,6 +1,9 @@
 package com.example.pawsandlearn;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,10 +23,14 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+//import java.util.Random;
 
 public class QuizActivity extends AppCompatActivity {
     RequestQueue queue;
-    ArrayList<JSONObject> catBreeds, dogBreeds;
+    ArrayList<JSONObject> catBreeds, dogBreeds, questions;
+    int qCount=0;
+    ImageView imgBreed;
+    Button btnRed, btnBlue, btnYellow, btnGreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +43,14 @@ public class QuizActivity extends AppCompatActivity {
         });
         dogBreeds=new ArrayList<>();
         catBreeds=new ArrayList<>();
+        questions = new ArrayList<>();
+
+        imgBreed = findViewById(R.id.imgBreed);
+        btnRed = findViewById(R.id.btnRed);
+        btnBlue = findViewById(R.id.btnBlue);
+        btnYellow = findViewById(R.id.btnYellow);
+        btnGreen = findViewById(R.id.btnGreen);
+
         queue = Volley.newRequestQueue(this);
         String url1  = "https://api.thedogapi.com/v1/breeds?api_key=live_ZTcjNqmEYLr05bs1a5FVB3bXD5geERNEhKgHLiDTgpg5dNOXQSEjvTtARtKYZeM2";
         String url2 = "https://api.thecatapi.com/v1/breeds?api_key=live_VlMdBZ0vyekrJGwlOn2X79pqhW2X6O2vhOeWpM77dRWjtRwITvhlun6ZMBsZHGCX";
@@ -45,8 +61,17 @@ public class QuizActivity extends AppCompatActivity {
                 dogBreeds.clear();
                 JSONArray res=(JSONArray) response;
                 for(int i=0;i<res.length();i++){
-                    dogBreeds.add((JSONObject) res.get(i));
+                    JSONObject dogObj = response.getJSONObject(i);
+                    String dogName = dogObj.getString("name");
+                    String dogURL = response.getJSONObject(i).getJSONObject("image").getString("url");
+
+                    JSONObject dogData = new JSONObject();
+                    dogData.put("name", dogName);
+                    dogData.put("image", dogURL);
+
+                    dogBreeds.add(dogData);
                 }
+                start();
             } catch (JSONException e) {
                 throw new RuntimeException(e);
             }
@@ -75,4 +100,115 @@ public class QuizActivity extends AppCompatActivity {
         queue.add(dogRequest);
         queue.add(catRequest);
     }
+
+    private void start(){
+        if(dogBreeds.size()<5){
+            return;
+        }
+
+        questions.clear();
+        //loops 5 times adding 5 dog breeds to questions array list
+        for(int i=0; i<5; i++){
+            //Random random = new Random();
+            //int randInd = random.nextInt(dogBreeds.size());
+            int randInd = (int) (Math.random() * dogBreeds.size());
+            //questions.add(dogBreeds.get(randInd));
+            questions.add(dogBreeds.get(randInd)); //adds a random index of the dog breeds to the questions array list
+        }
+
+        //do the same for cat breeds
+        //note: we do not need to shuffle the different breeds and orders around since they are already randomly picked
+        System.out.println("Questions size: " + questions.size());
+        nextQ();
+    }
+
+    private void nextQ(){
+        try{
+            JSONObject currentQ = questions.get(qCount);
+            String name = currentQ.getString("name");
+            String imageURL = currentQ.getString("image");
+            Picasso.get().load(imageURL).into(imgBreed);
+
+            String[] multChoice = new String[4];
+            int rightAnswer = (int)(Math.random()*4);
+            //Random random = new Random();
+            //int rightAnswer = random.nextInt(4);
+            multChoice[rightAnswer] = name;
+
+            //generates the random mult choice answers for the rest
+            for(int i = 0; i<4;i++){
+                if(i==rightAnswer){
+                    continue; //skips the correct answer so that it doesnt change it
+                }
+                boolean isDogQ = qCount < 5;
+                ArrayList<JSONObject>breedList;
+                if(isDogQ){
+                    breedList = dogBreeds;
+                } else {
+                    breedList = catBreeds;
+                }
+
+                while(true){
+                    //Random random2 = new Random();
+                    //int randomIndex = random2.nextInt(breedList.size());
+                    int randomIndex = (int)(Math.random()*breedList.size());
+                    String randomBreed = breedList.get(randomIndex).getString("name");
+                    if (!randomBreed.equals(name)) {
+                        multChoice[i] = randomBreed;
+                        break;
+                    }
+                }
+            }
+            btnRed.setText(multChoice[0]);
+            btnBlue.setText(multChoice[1]);
+            btnYellow.setText(multChoice[2]);
+            btnGreen.setText(multChoice[3]);
+
+            // Button click logic
+            btnRed.setOnClickListener(v -> checkAnswer(multChoice[0], name));
+            btnBlue.setOnClickListener(v -> checkAnswer(multChoice[1], name));
+            btnYellow.setOnClickListener(v -> checkAnswer(multChoice[2], name));
+            btnGreen.setOnClickListener(v -> checkAnswer(multChoice[3], name));
+
+        } catch (JSONException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void checkAnswer(String selected, String correct) {
+        if (selected.equals(correct)) {
+            //txtQuestion.setText("Correct!");
+            System.out.print("Correct!");
+        } else {
+            //txtQuestion.setText("Wrong! It was " + correct);
+            System.out.println("Incorrect! It was" + correct);
+        }
+
+        qCount++;
+        nextQ();
+    }
+
+    /*
+    private void yellowClick(View v){
+        checkAnswer(multChoice[2], name);
+    }
+
+    private void greenClick(View v){
+        checkAnswer(multChoice[3], name);
+    }
+
+    private void blueClick(View v){
+        checkAnswer(multChoice[1], name);
+    }
+
+    private void redClick(View v){
+        checkAnswer(multChoice[0], name);
+    }
+
+    private void prevClick(View v){
+        finish();
+    }
+
+     */
+
 }
